ef53200da4d1ab3795ec004825a4f82e2e3dc7d3
2013136082_R-pet'sSafeHabitat <kutmicro@gmail.com>
2016-11-29 12:33:05 +0900

    Update RGB_LED.ino

--- src/RGB_LED/RGB_LED.ino
+++ src/RGB_LED/RGB_LED.ino
@@ -1,13 +1,13 @@
-int ledRed = 11;//rgb led의 red부분 핀 설정 
-int ledGreen = 10;//rgb led의 green부분 핀 설정
-int ledBlue = 9;//rgb led의 blue부분 핀 설정
+int redPin = 9;//rgb led의 red부분 핀 설정 
+int greenPin = 10;//rgb led의 green부분 핀 설정
+int bluePin = 11;//rgb led의 blue부분 핀 설정
 
 void setColor(int red, int green, int blue);
 void setup()
 {
- pinMode(ledRed, OUTPUT);    //각 데이터 핀을 출력으로 설정
- pinMode(ledGreen, OUTPUT);
- pinMode(ledBlue, OUTPUT); 
+ pinMode(redPin, OUTPUT);    //각 데이터 핀을 출력으로 설정
+ pinMode(greenPin, OUTPUT);
+ pinMode(bluePin, OUTPUT); 
 }
  
 void loop()
@@ -22,7 +22,7 @@ void loop()
 void setColor(int red, int green, int blue) //각각 인자를 받아 색을 출력하는 함수
 {
   //analogWrite는 Duty rate를 조절하여 LED가 출력하는 밝기 조절 가능
-  analogWrite(ledRed, red);   //red 핀의 값 입력   
-  analogWrite(ledGreen, green); //green 핀의 값 입력
-  analogWrite(ledBlue, blue);     //blue 핀의 값 입력
+  analogWrite(redPin, red);   //red 핀의 값 입력   
+  analogWrite(greenPin, green); //green 핀의 값 입력
+  analogWrite(bluePin, blue);     //blue 핀의 값 입력
 }
